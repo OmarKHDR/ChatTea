1594e340e896d595a9029de045dd19914f2025b5
OmarKHDR <omarsliman37@gmail.com>
2025-01-12 20:07:10 +0200

    adding the html and css of the login

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,5 @@
-const sockect = io('ws://localhost:5000')
+const sendBtn = document.getElementById('sendButton')
+const chat = document.getElementById('messageArea')
 
 function sendMess() {
 	const text = document.getElementById('messageInput');
@@ -7,4 +8,29 @@ function sendMess() {
 		socket.emit(val);
 		text.value = "";
 	}
+}
+
+sendBtn.addEventListener('click', ()=>{
+	const message = document.getElementById('messageInput').value
+	console.log(message)
+	if (message) {
+		const childMessage = createMessageElement(message, 'sent-message');
+		console.log(childMessage)
+		chat.appendChild(childMessage);
+	}
+})
+
+function createMessageElement(message, className) {
+	const text = document.createElement('div');
+	text.setAttribute('class', className);
+	text.innerText = message;
+	const time = document.createElement('div');
+	time.setAttribute('class', 'message-time');
+	const t = new Date();
+	const nighty = (t.getHours() / 12) ? 'pm' : 'am';
+	const date = `${t.getHours() % 12}:${t.getMinutes()} ${nighty}`;
+	time.textContent = date;
+	text.appendChild(time);
+	//text.style.width = message.len;
+	return text;
 }
